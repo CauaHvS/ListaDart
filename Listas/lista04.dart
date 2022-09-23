0f7d3ea50b8592169db2b91998d50501412be869
Cauã Henrique Viana Salgado <71525414+CauaHvS@users.noreply.github.com>
2022-09-23 22:01:33 +0000

    Foi

--- Listas/lista04.dart
+++ Listas/lista04.dart
@@ -5,13 +5,10 @@ class Conta_bancaria{
   String? _nome_cliente;
   double? _saldo;
   double? _limite;
-
-
   
-  
-  Conta_bancaria(this._numero_conta, this._nome_cliente, this._saldo, this._limite);
+ Conta_bancaria(this._numero_conta, this._nome_cliente, this._saldo, this._limite);
 
-get numero_conta => _numero_conta;
+ get numero_conta => _numero_conta;
  set numero_conta(num_conta) => _numero_conta;
  get nome_cliente => _nome_cliente;
  set nome_cliente(nome) => _nome_cliente;
@@ -21,18 +18,18 @@ get numero_conta => _numero_conta;
  set limite(valor) => _limite;
 
   sacar(valor_saque){ 
-    if(valor_saque<saldo){
+    if(valor_saque < saldo){
       saldo = saldo - valor_saque;
       print("$nome_cliente você acaba de sacar $valor_saque");
     }else{
-      print("Saldo Insuficiente!");
+      print("Saldo Insuficiente!\n O seu saldo é de $saldo");
     }
-    
+    return saldo;
   }
 
   depositar(valor_deposito){
     saldo = saldo + valor_deposito;
-    print("$nome_cliente você acaba de depositar R\$ $valor_deposito.");
+    print("$nome_cliente você acaba de depositar R\$ $valor_deposito\n O seu saldo é de $saldo");
     return saldo;
   }
 
@@ -41,7 +38,15 @@ get numero_conta => _numero_conta;
   }
 
   void imprimirConta(){
-    
+
+    stdout.write("\n===================");
+    stdout.write("\n Extrato Bancario ");
+    stdout.write("\n===================");
+    stdout.write("\nNome: $nome_cliente");
+    stdout.write("\nNumero da conta: $numero_conta");
+    stdout.write("\nSaldo: $saldo");
+    stdout.write("\nLimite: $limite");
+    stdout.write("\n===================");
   }
 
  
@@ -52,17 +57,20 @@ get numero_conta => _numero_conta;
 
 void main(){
 
+  //List<Conta_bancaria> Contas = List.empty(growable: true);
   int? opcao;
-  int numconta = 1;
+  int numconta;
+  var conta;
+  
   do{
     
-    stdout.write("1. Abrir Conta\n");
+    stdout.write("\n1. Abrir Conta\n");
     stdout.write("2.Sacar\n");
     stdout.write("3.Depositar\n");
     stdout.write("4.Transferir\n");
     stdout.write("5.Imprimir conta\n");
     stdout.write("6.Sair\n");
-    var opcao = int.parse(stdin.readLineSync()!);
+    opcao = int.parse(stdin.readLineSync()!);
     
     switch(opcao){
       
@@ -83,18 +91,34 @@ void main(){
         var limite_cliente = double.parse(stdin.readLineSync()!);
         stdout.write("\n");
         
-        var conta = Conta_bancaria(numero_conta, nome_cliente, saldo_cliente, limite_cliente);
+        conta = Conta_bancaria(numero_conta, nome_cliente, saldo_cliente, limite_cliente);
         stdout.write("Conta Aberta!\n");
-        
-        numconta++;
-        print("$numconta");
         break;
       case 2:
-        print("Sacando");
+        stdout.write("Informe o valor que vai sacar:");
+        var valor_saque = double.parse(stdin.readLineSync()!);
+        stdout.write("\n");
+        conta.sacar(valor_saque);
+        break;
+      case 3:
+        stdout.write("Informe o valor que vai depositar:");
+        var valor_deposito = double.parse(stdin.readLineSync()!);
+        stdout.write("\n");
+        conta.depositar(valor_deposito);
+        break;
+      case 4:
+        conta.transferir();
+        break;
+      case 5:
+        conta.imprimirConta();
+        break;
+      case 6:
+        print("Saindo!");
+        break;
+      default:
+        break;
         
     }
-    
   }while(opcao != 6);
-  
   
 }
